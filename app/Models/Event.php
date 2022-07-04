2cc1c90e6c900a74da083d2b89bfee3e9e5af05d
Vinicius Hansen D Agostini <94183727+vidagostini1998@users.noreply.github.com>
2022-07-04 12:52:13 -0300

    Upload files and directories

---
+++ app/Models/Event.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $casts = ['items'=>'array'];
+
+    protected $dates = ['date'];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+    protected $guarded = [];
+
+    public function users(){
+        return $this->belongsToMany('App\Models\User');
+    }
+}
